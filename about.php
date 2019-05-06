82e8300eae83441ed04cd760689cc251f50d6d92
wei <weiwei198328@gmail.com>
2019-05-07 00:50:25 +0800

    模板改造初始化

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: about
+ */
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+      关于我们
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php
+get_footer();
---
+++ solutions.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+      解决方案
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php
+get_footer();
