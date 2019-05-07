d3b2cf5f12408f3d740029a137cb9af0230d621a
wei <weiwei198328@gmail.com>
2019-05-07 23:51:43 +0800

    同步最新的图片

--- solutions.php
+++ solutions.php
@@ -13,32 +13,32 @@ get_header();
 			</div>
 			<div class="solutions-1">
 				<div class="solution-flex">
-					<img src="../assets/solutions_1.jpg" alt class="sotxt">
-					<img src="../assets/solutions_1s.jpg" alt class="sohover">
+					<div class="sotxt"></div>
+					<div class="sohover"></div>
 				</div>
 			</div>
 			<div class="solutions-2">
 				<div class="solution-flex">
-					<img src="../assets/solutions_2s.jpg" alt class="sohover">
-					<img src="../assets/solutions_2.jpg" alt class="sotxt">
+					<div class="sohover"></div>
+					<div class="sotxt"></div>
 				</div>
 			</div>
 			<div class="solutions-3">
 				<div class="solution-flex">
-					<img src="../assets/solutions_3.jpg" alt class="sotxt">
-					<img src="../assets/solutions_3s.jpg" alt class="sohover">
+					<div class="sotxt"></div>
+					<div class="sohover"></div>
 				</div>
 			</div>
 			<div class="solutions-4">
 				<div class="solution-flex">
-					<img src="../assets/solutions_4s.jpg" alt class="sohover">
-					<img src="../assets/solutions_4.jpg" alt class="sotxt">
+					<div class="sohover"></div>
+					<div class="sotxt"></div>
 				</div>
 			</div>
 			<div class="solutions-5">
 				<div class="solution-flex">
-					<img src="../assets/solutions_5.jpg" alt class="sotxt">
-					<img src="../assets/solutions_5s.jpg" alt class="sohover">
+					<div class="sotxt"></div>
+					<div class="sohover"></div>
 				</div>
 			</div>
 		</div>
